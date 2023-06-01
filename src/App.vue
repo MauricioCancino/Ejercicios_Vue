453510a124dab1159db2e73590278e87bc712d44
Mauricio <cancinocontrerasm@gmail.com>
2023-06-01 08:52:17 -0400

    ejemplo sesion3

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand">Tienda Bit-game</a>  
+  </div>
+</nav>
+   
+   <tabla :games="games" @delete="eliminar"/> 
+
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Games from "@/components/Games.vue"
 
 export default {
-  name: 'App',
+  name: 'app',
+  data () {
+    return {
+      
+      games:["Arena of valor", "Pokemon Unite", "Donkey Kong", "Heroes of the storm", "Valorant", "Genshin Impact"],
+
+    }
+  },
+  methods: {
+
+    eliminar (index) {
+      this.games.splice(index,1)
+    }
+
+  },
   components: {
-    HelloWorld
+    "tabla":Games
   }
 }
 </script>
@@ -23,6 +44,20 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: inline
+}
+
+.container {
+  border: solid 1px black;
+  padding: 0;
+}
+
+.container-fluid {
+    
+    justify-content: center;
+}
+
+nav {
+  background-color: gray;
 }
 </style>
---
+++ src/components/Games.vue
@@ -0,0 +1,56 @@
+<template>
+     <div class="container">
+          
+     <table class="table">
+  <thead>
+    <tr>
+      <th scope="col-3">#</th>
+      <th scope="col-3">{{title1}}</th>
+      <th scope="col-3">{{title2}}</th>
+      
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(game, index) in games" :key="game.index">
+      <th scope="row"> {{index+1}}</th>
+      <td>
+        {{game}}
+      </td>
+      <button type="button" @click= "eliminar(index)" class="btn btn-danger">Eliminar</button>
+    </tr>
+  
+  </tbody>
+</table>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'game-component',
+    props: {games: {
+        type: Array,
+        required: true
+    }},
+    data: function(){
+        return {
+      title1 : "Juego",
+      title2 : "Eliminar juego de tu lista",}
+        
+    },
+    
+    methods: {
+        eliminar (index) {
+            this.$emit ("delete", index)
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+    .conntainer {
+        margin: 0;
+        padding: 0;
+        
+    }
+</style>
